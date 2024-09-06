f922fb4ff4c7b03ad3c22fced9f6cd1cf6047783
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2024-09-06 12:26:53 +0200

    Update SelectLink.tsx

--- app/components/SelectLink.tsx
+++ app/components/SelectLink.tsx
@@ -18,6 +18,7 @@ function SelectLink({ type, podcast, episode }: { type: "podcast" | "episode", p
   }
 
   const handleMouseEnter = () => {
+    console.log(podcast);
     setIsHovered(true);
   };
 
