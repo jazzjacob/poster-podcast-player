6b58e68558ac5ec8a3a76cf266f30bc129ce759f
Jacob Reinikainen <80760206+jazzjacob@users.noreply.github.com>
2025-11-11 15:29:19 +0100

    ios safari zoom in fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,12 @@ export const metadata: Metadata = {
   description: "Made by Jacob",
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
